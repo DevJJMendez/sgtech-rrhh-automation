28a697351fa1bb6a38b53b90075d44c57b56d076
JJMendez <DevJJMendez@gmail.com>
2025-05-12 08:46:00 -0500

    feat(WelcomeEmail class): create an email class to send emails

---
+++ app/Mail/WelcomeEmail.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\InvitationLink;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Termwind\Components\Dd;
+
+class WelcomeEmail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+    public $invitationLink;
+    public function __construct($invitationLink)
+    {
+        $this->invitationLink = $invitationLink;
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Bienvenido a SGTech',
+        );
+    }
+    public function content(): Content
+    {
+
+        $roleView = match ($this->invitationLink) {
+            'aprendiz' => 'emails.aprendiz',
+            'colaborador' => 'emails.colaborador',
+            'freelancer' => 'emails.freelancer',
+            default => 'emails.default',
+        };
+        return new Content(
+            view: $roleView,
+            with: ['invitation' => $this->invitationLink]
+        );
+    }
+    public function attachments(): array
+    {
+        return [];
+    }
+}

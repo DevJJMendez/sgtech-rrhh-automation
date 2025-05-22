501cd750a2490c95c3f51b6dd2dc3c07c7f350a1
JJMendez <DevJJMendez@gmail.com>
2025-05-22 09:32:32 -0500

    refactor(Migrations)

--- database/migrations/2025_04_15_180538_create_invitation_links_table.php
+++ database/migrations/2025_04_15_180538_create_invitation_links_table.php
@@ -10,7 +10,8 @@ return new class extends Migration {
         Schema::create('invitation_links', function (Blueprint $table) {
             $table->uuid();
             $table->string('email');
-            $table->enum('role', ['aprendiz', 'colaborador', 'freelancer']);
+            $table->unsignedTinyInteger('fk_collaborator_role_id');
+            $table->foreign('fk_collaborator_role_id')->references('collaborator_role_id')->on('collaborator_roles');
             // $table->timestamp('used_at')->nullable();
             // $table->timestamp('expires_at');
             $table->timestamps();

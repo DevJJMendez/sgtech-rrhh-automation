3a86f48eeb8350a8786b3c3bf5a5cb7a13cd1bb5
JJMendez <DevJJMendez@gmail.com>
2025-05-22 09:27:45 -0500

    feat(Collaborator Roles)

---
+++ database/migrations/2025_04_14_201017_create_collaborator_roles_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collaborator_roles', function (Blueprint $table) {
+            $table->unsignedTinyInteger('collaborator_role_id', true);
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collaborator_roles');
+    }
+};

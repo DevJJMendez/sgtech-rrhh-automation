1424729fb1dcb13eda4b1ae72ae6492814022eaf
JJMendez <DevJJMendez@gmail.com>
2025-05-23 11:21:50 -0500

    idk

--- resources/views/forms/register.blade.php
+++ resources/views/forms/register.blade.php
@@ -1,21 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>How to Create Multi Step Form in Laravel 11- Techsolutionstuff</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
 </head>
 
-<body>
-    <form action="">
-        <input type="text">
-        <input type="text">
-        <input type="text">
-        <input type="text">
-        <input type="text">
-        <input type="text">
-    </form>
+<body style="background-color: #f3fdf3">
+
+    <div class="container">
+        <h2> Laravel 11 Multi Step Form Example - Techsolutionstuff</h2>
+        @yield('content')
+    </div>
+
 </body>
 
 </html>

de7eed30bd020883df3e0b84dccc1aace1574c3c
JJMendez <DevJJMendez@gmail.com>
2025-06-04 15:34:38 -0500

    feat(models)

--- app/Models/EmergencyContact.php
+++ app/Models/EmergencyContact.php
@@ -10,6 +10,7 @@ class EmergencyContact extends Model
 {
     use HasFactory;
     protected $primaryKey = 'emergency_contact_id';
+    protected $guarded = [];
     public function personaldata(): BelongsTo
     {
         return $this->belongsTo(PersonalData::class, 'fk_personal_data_id', 'personal_data_id');
--- app/Models/FamilyData.php
+++ app/Models/FamilyData.php
@@ -10,6 +10,7 @@ class FamilyData extends Model
 {
     use HasFactory;
     protected $primaryKey = 'family_data_id';
+    protected $guarded = [];
     public function personaldata(): BelongsTo
     {
         return $this->belongsTo(PersonalData::class, 'fk_personal_data_id', 'personal_data_id');

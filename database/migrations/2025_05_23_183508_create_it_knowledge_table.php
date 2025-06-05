846e83868de3501efb82a9211a29bb68311e4589
JJMendez <DevJJMendez@gmail.com>
2025-06-05 09:49:36 -0500

    refactor(migrations)

--- database/migrations/2025_05_23_183508_create_it_knowledge_table.php
+++ database/migrations/2025_05_23_183508_create_it_knowledge_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
             $table->unsignedBigInteger('fk_personal_data_id');
             $table->foreign('fk_personal_data_id')->references('personal_data_id')->on('personal_data');
             $table->string('technology');
-            $table->string('level'); // basico - intermedio - avanzado
+            $table->enum('level', ['basic', 'intermediate', 'advanced']); // basico - intermedio - avanzado
             $table->timestamps();
         });
     }

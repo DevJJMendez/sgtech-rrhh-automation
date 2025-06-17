11c30443604910532ae33e9cb41a349f1b4008a8
JJMendez <DevJJMendez@gmail.com>
2025-06-17 09:02:18 -0500

    feat(frontend hiring form)

--- resources/views/emails/aprendiz.blade.php
+++ resources/views/emails/aprendiz.blade.php
@@ -25,3 +25,34 @@
 </body>
 
 </html>
+Buen día estimados aprendiz
+
+En primer lugar, quiero felicitarle y darle la bienvenida a SERVIGUIDE BPO SAS. Es una gran empresa y estoy seguro de
+que su incorporación, fortalecerá al equipo.
+
+Para iniciar el proceso de contratación, es necesario que envíe la siguiente documentación y así proceder con los
+trámites a su incorporación a la mayor brevedad posible. Importante que cada documento que adjunto lleve el mismo nombre
+y numero solicitado.
+
+El siguiente enlace lo llevara a un formularia en el cual diligenciara los datos de la Ficha de Contratación y los
+documentos requeridos
+
+1. Fotocopia de la cédula de ciudadanía ampliada al 150%
+2. Certificación de afiliación a EPS
+6. Certificación bancaria
+7. Antecedentes policía, procuraduría y contraloría (bajar de internet) (PDF)
+8. Documento adjunto (Ficha de Contratación)
+9. Fotografía para la firma de correo
+
+Cuando envíe la documentación, copie a todos los incluidos en el correo. Por favor confirmar la recepción de este correo
+y quedamos atentos a cualquier inquietud.
+
+Muchas gracias.
+
+
+
+¡SALUDOS!
+
+
+
+Att.,
--- resources/views/layouts/register.blade.php
+++ resources/views/layouts/register.blade.php
@@ -11,13 +11,6 @@
 </head>
 
 <body>
-    @if ($invitation->collaboratorRole->name === 'Freelancer')
-        {{ $invitation->collaboratorRole->name }}
-    @elseif ($invitation->collaboratorRole->name === 'Aprendiz')
-        {{ $invitation->collaboratorRole->name }}
-    @elseif ($invitation->collaboratorRole->name === 'Colaborador')
-        {{ $invitation->collaboratorRole->name }}
-    @endif
     <div class="form-container">
         <form action="{{ route('hiring.post') }}" method="POST">
             @csrf
@@ -549,6 +542,189 @@
                         </div>
                     </div>
                 </fieldset>
+                @if ($invitation->collaboratorRole->name === 'Freelancer')
+                    <fieldset class="form-section">
+                        <legend>Documentos requeridos</legend>
+                        <div class="input-row">
+                            <div class="input-group input-small">
+                                <label for="">Fotocopia de la cédula de ciudadanía ampliada al 150%</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Certificación bancaria</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Antecedentes policía, procuraduría y contraloría (bajar de
+                                    internet) (PDF)</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Fotografía para la firma de correo</label>
+                                <input type="file" name="technology">
+                            </div>
+                        </div>
+                    </fieldset>
+                @elseif ($invitation->collaboratorRole->name === 'Aprendiz')
+                    {{ $invitation->collaboratorRole->name }}
+                    <fieldset class="form-section">
+                        <legend>Documentos requeridos</legend>
+                        <div class="input-row">
+                            <div class="input-group input-small">
+                                <label for="">Fotocopia de la cédula de ciudadanía ampliada al 150%</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Certificación de afiliación a EPS</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Certificación bancaria</label>
+                                <input type="file" name="technology">
+                            </div>
+                        </div>
+                        <div class="input-row">
+                            <div class="input-group input-small">
+                                <label for="">Antecedentes policía, procuraduría y contraloría</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Documento adjunto (Ficha de Contratación)</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Fotografía para la firma de correo</label>
+                                <input type="file" name="technology">
+                            </div>
+                        </div>
+                    </fieldset>
+                @elseif ($invitation->collaboratorRole->name === 'Colaborador')
+                    {{ $invitation->collaboratorRole->name }}
+                    <fieldset class="form-section">
+                        <legend>Documentos requeridos</legend>
+                        <div class="input-row">
+                            <div class="input-group input-small">
+                                <label for="">Fotocopia de la cédula de ciudadanía ampliada al 150%</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Certificados de estudios realizados</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Dos últimas certificaciones laborales</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Certificación de afiliación a EPS</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Certificación de afiliación a Fondo de Pensiones</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Certificación bancaria</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Antecedentes policía, procuraduría y contraloría (bajar de
+                                    internet) (PDF)</label>
+                                <input type="file" name="technology">
+                            </div>
+                            <div class="input-group input-small">
+                                <label for="">Fotografía para la firma de correo</label>
+                                <input type="file" name="technology">
+                            </div>
+
+                            <fieldset class="form-section">
+                                <legend class="">DOCUMENTOS PARA AFILIACION DE BENEFICIARIOS</legend>
+                                <fieldset class="form-section">
+                                    <legend>Hijos</legend>
+                                    <div class="input-row">
+                                        <div class="input-group input-small">
+                                            <label for="">Registro civil de nacimiento (debe indicar el número
+                                                NUIP)</label>
+                                            <input type="file" name="technology">
+                                        </div>
+                                        <div class="input-group input-small">
+                                            <label for="">Fotocopia de la tarjeta de identidad (Para hijos
+                                                mayores de 7 años)</label>
+                                            <input type="file" name="technology">
+                                        </div>
+                                        <div class="input-group input-small">
+                                            <label for="">Niños mayores de 12 años, presentar certificación del
+                                                plantel donde recibe formación académica.</label>
+                                            <input type="file" name="technology">
+                                        </div>
+                                        <div class="input-group input-small">
+                                            <label for="">Certificado laboral del conyugue indicando: salario,
+                                                tiempo laborado, y si recibe subsidio familiar por parte de la caja a la
+                                                cual se encuentre afiliado.</label>
+                                            <input type="file" name="technology">
+                                        </div>
+                                        <div class="input-group input-small">
+                                            <label for="">Copia de la cédula del cónyuge.</label>
+                                            <input type="file" name="technology">
+                                        </div>
+                                        <div class="input-group input-small">
+                                            <label for="">En caso de ser madre cabeza de familia, la
+                                                trabajadora debe presentar una declaración extrajuicio argumentando su
+                                                condición.</label>
+                                            <input type="file" name="technology">
+                                        </div>
+                                    </div>
+                                </fieldset>
+                                <fieldset class="form-section">
+                                    <legend>Conyuge</legend>
+                                    <div class="input-row">
+                                        <div class="input-group input-small">
+                                            <label for="">Fotocopia de cédula de ciudadanía</label>
+                                            <input type="file" name="technology">
+                                        </div>
+                                        <div class="input-group input-small">
+                                            <label for="">Registro civil de matrimonio o declaración
+                                                extrajuicio de convivencia en unión libre</label>
+                                            <input type="file" name="technology">
+                                        </div>
+                                        <div class="input-group input-small">
+                                            <label for="">Certificado laboral de la empresa donde
+                                                labora.</label>
+                                            <input type="file" name="technology">
+                                        </div>
+                                    </div>
+                                </fieldset>
+                                <fieldset class="form-section">
+                                    <legend>Padres</legend>
+                                    <div class="input-row">
+                                        <div class="input-group input-small">
+                                            <label for="">Fotocopia de la cédula de ciudadanía</label>
+                                            <input type="file" name="technology">
+                                        </div>
+                                        <div class="input-group input-small">
+                                            <label for="">Registro civil de nacimiento del trabajador</label>
+                                            <input type="file" name="technology">
+                                        </div>
+                                        <div class="input-group input-small">
+                                            <label for="">Certificación del SISBEN o EPS que sea beneficiario
+                                                del trabajador</label>
+                                            <input type="file" name="technology">
+                                        </div>
+                                        <div class="input-group input-small">
+                                            <p>
+                                                * Si los padres son menores de 60 años, podrán ser afiliados a la caja
+                                                de
+                                                compensación familiar para acceder a los servicios que presta, se debe
+                                                anexar
+                                                únicamente fotocopia de cédula de padre y madre del colaborador.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </fieldset>
+                        </div>
+                    </fieldset>
+                @endif
             </fieldset>
             <button type="submit" class="btn-submit-form">Enviar</button>
         </form>

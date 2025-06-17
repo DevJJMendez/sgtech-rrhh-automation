91e77a974ba527aefa4745cb7fbf8f40622f46ec
JJMendez <DevJJMendez@gmail.com>
2025-06-17 09:11:18 -0500

    refactor(frontend hiring form)

---
+++ resources/views/components/aprendiz-files-form.blade.php
@@ -0,0 +1,31 @@
+<fieldset class="form-section">
+    <legend>Documentos requeridos</legend>
+    <div class="input-row">
+        <div class="input-group input-small">
+            <label for="">Fotocopia de la cédula de ciudadanía ampliada al 150%</label>
+            <input type="file" name="technology">
+        </div>
+        <div class="input-group input-small">
+            <label for="">Certificación de afiliación a EPS</label>
+            <input type="file" name="technology">
+        </div>
+        <div class="input-group input-small">
+            <label for="">Certificación bancaria</label>
+            <input type="file" name="technology">
+        </div>
+    </div>
+    <div class="input-row">
+        <div class="input-group input-small">
+            <label for="">Antecedentes policía, procuraduría y contraloría</label>
+            <input type="file" name="technology">
+        </div>
+        <div class="input-group input-small">
+            <label for="">Documento adjunto (Ficha de Contratación)</label>
+            <input type="file" name="technology">
+        </div>
+        <div class="input-group input-small">
+            <label for="">Fotografía para la firma de correo</label>
+            <input type="file" name="technology">
+        </div>
+    </div>
+</fieldset>
---
+++ resources/views/components/freelancer-files-form.blade.php
@@ -0,0 +1,22 @@
+<fieldset class="form-section">
+    <legend>Documentos requeridos</legend>
+    <div class="input-row">
+        <div class="input-group input-small">
+            <label for="">Fotocopia de la cédula de ciudadanía ampliada al 150%</label>
+            <input type="file" name="technology">
+        </div>
+        <div class="input-group input-small">
+            <label for="">Certificación bancaria</label>
+            <input type="file" name="technology">
+        </div>
+        <div class="input-group input-small">
+            <label for="">Antecedentes policía, procuraduría y contraloría (bajar de
+                internet) (PDF)</label>
+            <input type="file" name="technology">
+        </div>
+        <div class="input-group input-small">
+            <label for="">Fotografía para la firma de correo</label>
+            <input type="file" name="technology">
+        </div>
+    </div>
+</fieldset>

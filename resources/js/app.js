25ddadef39dafd5c9dc7ff4ba321a2327f51eade
JJMendez <DevJJMendez@gmail.com>
2025-06-24 09:52:52 -0500

    feat(frontend spa)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,8 @@
 import './bootstrap';
+// import './sidebar-navigation';
+
 import { setupUserModal } from './user-modal';
+import { sidebarNavigation } from './sidebar-navigation';
+
 setupUserModal();
+sidebarNavigation();

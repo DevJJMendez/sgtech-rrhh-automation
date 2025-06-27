6d445b6ebc8a9f69963aa874cc9d1a3d0ad136ba
JJMendez <DevJJMendez@gmail.com>
2025-06-27 09:20:31 -0500

    feat(views): add error views

---
+++ resources/views/errors/link_already_used.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    {{-- <h2>Este enlace ya fue utilizado</h2>
+    <p>Si necesitas volver a acceder, solicita uno nuevo a Recursos Humanos.</p> --}}
+    <h2>Enlace no encontrado</h2>
+</body>
+
+</html>

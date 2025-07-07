a887426bfda1b80846f78b5900b8e5bad656de45
JJMendez <DevJJMendez@gmail.com>
2025-07-07 14:05:19 -0500

    feat(app name and logo)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'SGTech RRHH') }}</title>
+    <link rel="icon" href="{{ asset('images/sgtech-icon.png') }}" type="image/png">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">

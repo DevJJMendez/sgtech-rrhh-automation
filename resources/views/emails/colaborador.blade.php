4f476d758189a3ec2fb90ea7badfe3c02f72e003
JJMendez <DevJJMendez@gmail.com>
2025-07-16 13:46:09 -0500

    refactor(welcome email): internal regulation

--- resources/views/emails/colaborador.blade.php
+++ resources/views/emails/colaborador.blade.php
@@ -7,22 +7,43 @@
 </head>
 
 <body>
-    <p>Buen día</p>
+    <p>Buen día,</p>
 
-    <p>En primer lugar, quiero felicitarle y darle la bienvenida a SERVIGUIDE BPO SAS. Es una gran empresa y estoy
-        seguro
-        de que su incorporación, fortalecerá al equipo.</p>
+    <p>En primer lugar, quiero felicitarle y darle la bienvenida a <strong>SERVIGUIDE BPO SAS</strong>. Es una gran
+        empresa y estoy seguro de que su incorporación fortalecerá al equipo.</p>
 
     <p>Para iniciar el proceso de contratación, es necesario que registre su información, cargue los documentos
-        requeridos y así proceder con los trámites a su incorporación a la mayor brevedad posible.</p>
+        requeridos y así proceder con los trámites de su vinculación a la mayor brevedad posible.</p>
 
-    <p>El siguiente enlace lo llevara a un formularia en el cual diligenciara los datos de la <b>Ficha de Contratación y
-            los documentos</b> requeridos:
+    <p>El siguiente enlace lo llevará a un formulario donde deberá diligenciar los datos correspondientes a la
+        <strong>Ficha de Contratación</strong> y cargar los documentos requeridos:
     </p>
-    <p><a href="{{ $url }}" target="_blank">Ficha de contratación</a></p>
-    <p>Este enlace estará activo durante 3 días.</p>
 
-    <p>Saludos,<br>SGTech</p>
+    <p><a href="{{ $url }}" target="_blank">→ Acceder al formulario de contratación</a></p>
+
+    <p><strong>Nota importante:</strong> este enlace estará activo durante <strong>3 días</strong> desde la fecha de
+        envío del presente correo.</p>
+
+    <hr>
+
+    <p><strong>Documentación adicional:</strong></p>
+    <p>Adjunto a este correo encontrará la <strong>reglamentación interna</strong> de la empresa, la cual debe leer
+        cuidadosamente.</p>
+
+    <p>Además, para facilitar su integración, le compartimos los siguientes videos informativos:</p>
+    <ul>
+        <li><a href="https://youtu.be/AJLOi6u4fLM" target="_blank">RIT</a>
+        </li>
+        <li><a href="https://youtu.be/zpcZEJoDY-I" target="_blank">Política de Tratamiento de datos personales</a></li>
+        <li><a href="https://youtu.be/c6A5HscvUa8">Política de Alcohol y Drogas</a></li>
+        <li><a href="https://youtu.be/35GKxS1_AJU">Política de Desconexión Laboral</a></li>
+        <li><a href="https://youtu.be/hcMq_mMUcxM">Permisos, licencias e incapacidades</a></li>
+        <li><a href="https://youtu.be/kLxKFcW2jns">Política Trabajo en casa, teletrabajo y trabajo remoto</a></li>
+    </ul>
+
+    <p>Quedamos atentos a cualquier duda o requerimiento adicional.</p>
+
+    <p>Saludos cordiales,<br>Equipo de SGTech</p>
 </body>
 
 </html>
